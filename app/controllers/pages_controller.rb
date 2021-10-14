270a7bfd11ac250d9ea3e163986d8443903cb969
SusieG <susiegaribay33@gmail.com>
2021-10-13 19:59:20 -0500

    add initial letter and guessing game routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,28 @@ class PagesController < ApplicationController
   def name_own
     render json: {message: "hello"}  
   end
+  
+  def initial_letter
+    name_string = params["name"]
+    name_array = name_string.split(//)
+    first = name_array[0]
+
+    if first.downcase == "a"
+      render json: {message: "Hey, your name starts with the first letter of the alphabet!"}
+    else
+      render json: {message: "Your name is weird."}
+    end
+  end
+
+  def check_number
+    input = params["number"].to_i
+    if input == 12
+      render json: {message: "Correct!"}
+    elsif input < 12
+      render json: {message: "Aim higher."}
+    elsif input > 12
+      render json: {message: "Aim lower."}
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/my_name", controller: "pages", action: "name_own"
+  get "/first_letter", controller: "pages", action: "initial_letter"
+  get "/guess_number", controller: "pages", action: "check_number"
 end
