0f8d81b86a828d3dd01379e9ab8e5c11971a5ca3
SusieG <susiegaribay33@gmail.com>
2021-10-13 20:13:44 -0500

    modify routes syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/my_name", controller: "pages", action: "name_own"
-  get "/first_letter", controller: "pages", action: "initial_letter"
-  get "/guess_number", controller: "pages", action: "check_number"
+  get "/my_name" => "pages#name_own"
+  get "/first_letter" => "pages#initial_letter"
+  get "/guess_number" => "pages#check_number"
 end
